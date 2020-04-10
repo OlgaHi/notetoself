5dc9035bcf848807de1269c3a16ea66b03aa15fe
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2020-04-09 19:26:44 -0700

    testing rendering app component

---
+++ src/components/Note.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Note from './Note';
+
+const props = {note: {text: 'test note'}}
+
+
+
+describe('Note', () => {
+  let note = mount(<Note note {...props}/>);  
+
+  it('renders the note text', () => {
+      expect(note.find('p').text()).toEqual(props.note.text)
+  });
+});
